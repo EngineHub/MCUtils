6eb811de9c11b5c5340e1d122703989f41a44f0b
Madeline Miller <mnmiller1@me.com>
2023-12-11 21:25:13 +1000

    1.20.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,9 @@ repositories {
 }
 
 dependencies {
-    "minecraft"("com.mojang:minecraft:1.20.3")
+    "minecraft"("com.mojang:minecraft:1.20.4")
     "mappings"(project.the<LoomGradleExtensionAPI>().officialMojangMappings())
-    "modImplementation"("net.fabricmc:fabric-loader:0.15.0")
+    "modImplementation"("net.fabricmc:fabric-loader:0.15.1")
 
     "implementation"("com.squareup:javapoet:1.13.0")
 
